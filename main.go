a7b1c676aecd1ce2c230de56ede719b114f91f48
Shinji <shinji@shinji.rocks>
2022-04-03 21:02:59 +0800

    stop crawling!

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	migrateDatabase()
-	go t66ySearchKeywords("sm", "捆", "瘦", "SM", "虐")
+	// go t66ySearchKeywords("sm", "捆", "瘦", "SM", "虐")
 	http.ListenAndServe(":1443", getRouter())
 
 }
